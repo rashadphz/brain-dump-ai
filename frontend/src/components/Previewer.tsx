f2b502f29d4157b018ed822e56dbb7f75fcf5ca6
Rashad Philizaire <rashadphilizaire@gmail.com>
2023-05-04 22:01:53 -0400

    much better preview styling using chakra components

--- frontend/src/components/Previewer.tsx
+++ frontend/src/components/Previewer.tsx
@@ -1,10 +1,103 @@
 import "highlight.js/styles/github-dark-dimmed.css";
 import hljs from "highlight.js";
 import { useEffect } from "react";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Checkbox,
+  Heading,
+  Link,
+  ListItem,
+  UnorderedList,
+} from "@chakra-ui/react";
 
 import "../bearstyle.css";
-import Markdown from "marked-react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import ChakraUIRenderer, {
+  defaults,
+} from "chakra-ui-markdown-renderer";
+
+const baseHeadingProps = {
+  fontFamily: "Avenir Next, system-ui, sans-serif",
+  color: "#ccdce5",
+  fontWeight: 500,
+};
+
+const headerMap = new Map([
+  ["h1", "1.9em"],
+  ["h2", "1.65em"],
+  ["h3", "1.25em"],
+  ["h4", "1em"],
+  ["h5", "1em"],
+  ["h6", "1em"],
+]);
+
+const baseHeaders: any = {};
+
+for (const [header, fontSize] of headerMap) {
+  baseHeaders[header] = (props: any) => (
+    <Heading
+      fontSize={fontSize}
+      {...baseHeadingProps}
+      {...props}
+      as={header}
+    />
+  );
+}
+
+const baseTheme: typeof defaults = {
+  ...baseHeaders,
+  h1: (props) => (
+    <Heading
+      fontSize="1.9em"
+      lineHeight={2.5}
+      {...baseHeadingProps}
+      {...props}
+    />
+  ),
+  blockquote: (props: any) => (
+    <Box my={4} borderLeft="4px solid #44a2e5" pl={4} {...props} />
+  ),
+  ul: (props: any) => (
+    <UnorderedList {...props}>{props.children}</UnorderedList>
+  ),
+  li: (props) => {
+    const isTaskListItem = props.className === "task-list-item";
+    const { checked, children } = props;
+    if (isTaskListItem) {
+      return (
+        <ListItem listStyleType="none">
+          <Checkbox isChecked={checked || false}>{children}</Checkbox>
+        </ListItem>
+      );
+    }
+
+    return (
+      <ListItem
+        listStyleType="none"
+        _before={
+          isTaskListItem
+            ? {}
+            : {
+                content: "'•'",
+                color: "#44a2e5",
+                width: "2em",
+                marginRight: "0.5em",
+              }
+        }
+        {...props}
+      />
+    );
+  },
+  a: (props: any) => (
+    <Link color="#44a2e5" {...props}>
+      {props.children}
+    </Link>
+  ),
+  input: (props) => {
+    return <></>;
+  },
+};
 
 const Previewer = ({ markdown }: { markdown: string }) => {
   useEffect(() => {
@@ -21,7 +114,13 @@ const Previewer = ({ markdown }: { markdown: string }) => {
       fontFamily={"Avenir Next, system-ui, sans-serif"}
       backgroundColor="transparent"
     >
-      <Markdown>{markdown}</Markdown>
+      <ReactMarkdown
+        components={ChakraUIRenderer({
+          ...baseTheme,
+        })}
+        children={markdown}
+        remarkPlugins={[remarkGfm]}
+      />
     </Box>
   );
 };
