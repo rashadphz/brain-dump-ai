27c0db02f80953cb3ed6940b24f374521e7727e3
Rashad Philizaire <rashadphilizaire@gmail.com>
2023-05-05 14:02:32 -0400

    basic command modal component

--- frontend/src/components/CommandModal/index.tsx
+++ frontend/src/components/CommandModal/index.tsx
@@ -1,5 +1,13 @@
 import {
+  Box,
   Button,
+  HStack,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Kbd,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,6 +15,8 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  VStack,
+  Text,
 } from "@chakra-ui/react";
 import {
   useReduxDispatch,
@@ -14,6 +24,7 @@ import {
 } from "../../redux/hooks";
 import { handleClose } from "./commandModalSlice";
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const CommandModal = () => {
   const isOpen = useReduxSelector(
@@ -22,18 +33,50 @@ const CommandModal = () => {
   const dispatch = useReduxDispatch();
 
   return (
-    <Modal isOpen={isOpen} onClose={() => dispatch(handleClose())}>
+    <Modal
+      size="2xl"
+      isOpen={isOpen}
+      onClose={() => dispatch(handleClose())}
+    >
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <p>Modal body text goes here.</p>
+      <ModalContent
+        backgroundColor="gray.900"
+        color="white"
+        borderRadius="2xl"
+      >
+        <ModalBody px={1}>
+          <Box borderBottomWidth="1px" mb={5}>
+            <InputGroup>
+              <Input
+                placeholder="Search note or type command..."
+                border="none"
+                width="full"
+                shadow="none"
+                _focus={{ shadow: "none", border: "none" }}
+              />
+              <InputLeftElement pointerEvents="none">
+                <Icon as={AiOutlineSearch} color="gray.300" />
+              </InputLeftElement>
+            </InputGroup>
+          </Box>
+          <VStack align="start" spacing={2}></VStack>
         </ModalBody>
-        <ModalFooter>
-          <Button onClick={() => dispatch(handleClose())}>
-            Close
-          </Button>
+        <ModalFooter px={1} py={0}>
+          <HStack
+            px={3}
+            py={3}
+            borderTopWidth="1px"
+            width="full"
+            spacing={5}
+            justifyContent="space-between"
+          >
+            <Text fontWeight="medium" color="gray.500" fontSize="sm">
+              Suggestions <Kbd>TAB</Kbd>
+            </Text>
+            <Text fontWeight="medium" color="gray.500" fontSize="sm">
+              Open <Kbd>↵</Kbd>
+            </Text>
+          </HStack>
         </ModalFooter>
       </ModalContent>
     </Modal>
