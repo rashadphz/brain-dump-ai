87368e8d7c4f2a61aa823c452f6bab87e96013cf
Rashad Philizaire <rashadphilizaire@gmail.com>
2023-05-05 21:56:29 -0400

    removed unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import "./titlebar.css";
 import {
   Box,
   Button,
   Flex,
-  HStack,
-  IconButton,
-  Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
-  VStack,
-  useColorMode,
-  useColorModeValue,
   useMultiStyleConfig,
   useTab,
 } from "@chakra-ui/react";
@@ -27,58 +19,11 @@ import HelperSidebar from "./components/HelperSidebar";
 import NotesSidebar from "./components/NotesSidebar";
 
 import React from "react";
-import NoteService, { Note, NoteChangeType } from "./db/dbservice";
 import Editor from "./components/Editor";
 import Previewer from "./components/Previewer";
 import CommandModal from "./components/CommandModal";
-import { useReduxDispatch } from "./redux/hooks";
-import { globalNoteOpen } from "./features/notes/noteSlice";
-
-const debounce = (fn: Function, ms = 300) => {
-  let timeoutId: ReturnType<typeof setTimeout>;
-  return function (this: any, ...args: any[]) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn.apply(this, args), ms);
-  };
-};
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
-  const dispatch = useReduxDispatch();
-
-  const getNoteTitle = (mkdn: string): string => {
-    const match = mkdn.match(/# (.*)$/m);
-    return match ? match[1] : "Untitled";
-  };
-
-  const getNoteTags = (mkdn: string): string[] => {
-    const regex = /(?![-|>])#(\w+\b)+/g;
-    const matches = mkdn.match(regex);
-    return matches ? matches.map((m) => m.slice(1)) : [];
-  };
-
-  const saveNote = useCallback(
-    debounce((modifiedNote: Note) => {
-      if (!modifiedNote._id) return;
-      const title = getNoteTitle(modifiedNote.content);
-      const tags = getNoteTags(modifiedNote.content);
-
-      NoteService.updateNoteById(modifiedNote._id, {
-        ...modifiedNote,
-        title,
-        tags,
-      });
-    }, 1000),
-    []
-  );
-
-  useEffect(() => {
-    NoteService.getAllNotes().then((notes) => {
-      setNotes(notes);
-      dispatch(globalNoteOpen({ ...notes[0] }));
-    });
-  }, []);
-
   const CustomTab = React.forwardRef(
     (props: any, ref: React.Ref<any>) => {
       const tabProps = useTab({ ...props, ref });
